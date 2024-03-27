30e4989fd2bb94c193f2cef04b62d0cf4373a940
Pradyumna Deshpande <93860996+pradyumna03@users.noreply.github.com>
2024-03-27 17:15:14 -0500

    CovidProject.sql

--- CovidProject.sql
+++ CovidProject.sql
@@ -50,10 +50,83 @@ ORDER BY TotalDeathCount DESC;
 
 -- global numbers
 
+SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS DeathPercentage
+FROM covid.coviddeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2; 
 
+-- Total Population vs vaccines
+-- Percentage of Population that has recieved at least one Covid Vaccine
 
+SELECT 
+    cd.continent, 
+    cd.location, 
+    cd.date, 
+    cd.population, 
+    vc.new_vaccinations, SUM(new_vaccinations) AS RollingPeopleVaccinated
+FROM 
+    covid.coviddeaths cd
+JOIN 
+    covid.covidvaccinations vc ON cd.location = vc.location AND cd.date = vc.date
+WHERE 
+    cd.continent IS NOT NULL 
+ORDER BY 
+    location, date;
+    
+    
+-- using CTE to perform calculation
 
+WITH VaccinationSummary AS (
+    SELECT 
+        cd.continent, 
+        cd.location, 
+        cd.date, 
+        cd.population, 
+        vc.new_vaccinations
+    FROM 
+        covid.coviddeaths cd
+    JOIN 
+        covid.covidvaccinations vc ON cd.location = vc.location AND cd.date = vc.date
+    WHERE 
+        cd.continent IS NOT NULL
+)
+SELECT 
+    cd.continent, 
+    cd.location, 
+    cd.date, 
+    cd.population, 
+    vc.new_vaccinations, SUM(new_vaccinations) AS RollingPeopleVaccinated
+FROM 
+    VaccinationSummary
+GROUP BY 
+    continent, location, date, population, new_vaccinations
+ORDER BY 
+    location, date;
+    
+-- creating view for future visualization
 
-
-
-
+CREATE VIEW VaccinationSummaryView AS 
+WITH VaccinationSummary AS (
+	SELECT 
+        cd.continent, 
+        cd.location, 
+        cd.date, 
+        cd.population, 
+        vc.new_vaccinations
+    FROM 
+        covid.coviddeaths cd
+    JOIN 
+        covid.covidvaccinations vc ON cd.location = vc.location AND cd.date = vc.date
+    WHERE 
+        cd.continent IS NOT NULL
+)
+SELECT 
+    cd.continent, 
+    cd.location, 
+    cd.date, 
+    cd.population, 
+    vc.new_vaccinations, SUM(new_vaccinations) AS RollingPeopleVaccinated
+FROM 
+    VaccinationSummary
+GROUP BY 
+    continent, location, date, population, new_vaccinations;
